f0c78640fb9f8d6bb4621eac0d38e85a53aa1b11
wenyaohugithub <wenyaohu@gmail.com>
2020-08-09 10:49:10 -0400

    Fix syntax.

--- src/main/webapp/app/services/util/print.service.js
+++ src/main/webapp/app/services/util/print.service.js
@@ -5,13 +5,7 @@ angular.module('mlcccApp')
         this.print = function(title, content){
             var popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
             popupWin.document.open();
-            popupWin.document.write(`
-                <html>
-                    <head>
-                      <title>${title}</title>
-                    </head>
-                    <body onload="window.print();window.close()">${content}</body>
-                  </html>`
+            popupWin.document.write('<html> <head> <title>'+title+'</title> </head> <body onload="window.print();window.close()">'+content+'</body> </html>'
             );
             popupWin.document.close();
         };
